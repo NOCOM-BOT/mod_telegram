01f217fce631080032263a026d5903cc362b6192
Lê Quang Lâm <badaimweeb@protonmail.com>
2022-09-11 22:43:49 +0000

    debug because i don't know how telegram works

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,29 @@ cmc.on("api:login", async (call_from: string, data: {
         botToken: string;
     }
 }, callback: (error?: any, data?: any) => void) => {
-    
+    let bot = new Telegraf(data.loginData.botToken);
+
+    // Logging in
+    try {
+        await bot.launch()
+    } catch {
+        callback("Invalid bot token", {
+            success: false
+        });
+        return;
+    }
+
+    // Logged in, now we can start listening to messages
+    // Note that we don't use Telegram command (yet), so we'll listen for actual messages
+    // and send it to command handler.
+    bot.on("message", async (ctx) => {
+        logger.debug("telegram", JSON.parse(JSON.stringify(ctx)));
+    });
+
+    logger.info("telegram", `Interface ${data.interfaceID} logged in.`);
+    callback(null, {
+        success: true
+    });
 });
 
 cmc.on("api:logout", async (call_from: string, data: {
@@ -49,12 +71,12 @@ cmc.on("api:get_userinfo", async (call_from: string, data: {
     interfaceID: number,
     userID: string
 }, callback: (error?: any, data?: any) => void) => {
-    
+
 });
 
 cmc.on("api:get_channelinfo", async (call_from: string, data: {
     interfaceID: number,
     channelID: string
 }, callback: (error?: any, data?: any) => void) => {
-    
+
 });
