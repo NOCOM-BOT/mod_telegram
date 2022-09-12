99aa7e37280c0e174a90ed1c2935ec1252aa3a87
Lê Quang Lâm <badaimweeb@protonmail.com>
2022-09-12 00:49:13 +0000

    Send text messages to command handler

--- src/index.ts
+++ src/index.ts
@@ -47,8 +47,58 @@ cmc.on("api:login", async (call_from: string, data: {
     // Logged in, now we can start listening to messages
     // Note that we don't use Telegram command (yet), so we'll listen for actual messages
     // and send it to command handler.
-    bot.on("message", async (ctx) => {
-        logger.debug("telegram", JSON.stringify(ctx));
+    bot.on("text", async (ctx) => {
+        // Simple text.
+        cmc.callAPI("core", "send_event", {
+            eventName: "interface_message",
+            data: {
+                interfaceID: data.interfaceID,
+                interfaceHandlerName: "Telegram",
+
+                content: ctx.message.text,
+                attachments: [],
+                mentions: Object.fromEntries(
+                    (await Promise.all(
+                        ctx.message.entities
+                            ?.map?.(async e => {
+                                switch (e.type) {
+                                    case "mention":
+                                        // Resolve username to ID
+                                        let username = ctx.message.text.substring(e.offset + 1, e.offset + e.length);
+                                        let usernameChat = await ctx.telegram.getChat(username);
+                                        return [usernameChat.id, {
+                                            start: e.offset,
+                                            length: e.length
+                                        }];
+                                    case "text_mention":
+                                        return [e.user.id, {
+                                            start: e.offset,
+                                            length: e.length
+                                        }];
+                                    default:
+                                        return [];
+                                }
+                            }) ?? []
+                    )).filter(x => x.length)
+                ),
+
+                messageID: ctx.message.message_id.toString(),
+                formattedMessageID: `${ctx.botInfo.id}_${ctx.message.message_id}@Message@Telegram`,
+                channelID: ctx.chat.id.toString(),
+                formattedChannelID: `${ctx.chat.id}@Channel@Telegram`,
+                // Telegram doesn't have guilds, so we'll just use channel ID as guild ID.
+                guildID: ctx.chat.id.toString(),
+                formattedGuildID: `${ctx.chat.id}@Channel@Telegram`,
+                senderID: ctx.from.id.toString(),
+                formattedSenderID: `${ctx.from.id}@User@Telegram`,
+
+                additionalInterfaceData: {
+                    isDM: ctx.chat.type === "private",
+                    isReply: ctx.message.reply_to_message !== undefined,
+                    replyMessageID: ctx.message.reply_to_message?.message_id?.toString?.()
+                }
+            }
+        })
     });
 
     logger.info("telegram", `Interface ${data.interfaceID} logged in.`);
